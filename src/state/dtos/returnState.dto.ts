303ca95f63095d03f1f7ed468bf967d25869f73c
Yuri Sousa Oliveira <yuri.sousa42@hotmail.com>
2024-03-29 20:50:21 -0300

    (ADD) Separate DTO State City Adresses User

--- src/state/dtos/returnState.dto.ts
+++ src/state/dtos/returnState.dto.ts
@@ -3,7 +3,7 @@ import { StateEntity } from '../entities/state.entity';
 export class ReturnStateDto {
   name: string;
 
-  constructor(address: StateEntity) {
-    this.name = address.name;
+  constructor(state: StateEntity) {
+    this.name = state.name;
   }
 }
