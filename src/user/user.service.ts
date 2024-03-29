3457488600b159764825493f3e5b20c112f8a43c
Yuri Sousa Oliveira <yuri.sousa42@hotmail.com>
2024-03-29 18:44:01 -0300

    (ADD) Validation create Address FindById

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -25,5 +25,17 @@ export class UserService {
 
   async getAllUsers(): Promise<UserEntity[]> {
     return this.userRepository.find();
+  }
+
+  async findUserById(userId: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException(`UserId: ${userId}. Not Found`);
+
+    return user;
   }
 }
